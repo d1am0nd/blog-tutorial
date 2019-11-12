8270ea3452161eaa96c8433c5236bcf400459fcf
Dev Kordes <devkordes@gmail.com>
2019-11-11 20:01:27 -0500

    Renaming

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,55 +20,23 @@
                 margin: 0;
             }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
             .title {
-                font-size: 84px;
+                font-size: 2em;
+                margin-top: 0.5em;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            .container {
+                width: auto;
+                max-width: 680px;
+                padding: 0 15px;
+                margin-left: auto;
+                margin-right: auto;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div id="example"></div>
-            </div>
-        </div>
+        <!-- Begin page content -->
+        <main role="main" class="container" id="root"></main>
         <script src="{{ mix('/js/app.js') }}"></script>
     </body>
 </html>
