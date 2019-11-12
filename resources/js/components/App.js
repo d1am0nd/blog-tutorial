98d0212cf8b68a4692ed88cb89cd20980325da30
Dev Kordes <devkordes@gmail.com>
2019-11-11 20:01:27 -0500

    Renaming

---
+++ resources/js/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <>
+            <div className="title">Our blog</div>
+        </>
+    );
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
