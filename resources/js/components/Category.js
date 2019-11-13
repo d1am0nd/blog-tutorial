5b8afa739401b6e7694c6a2b30670ab2517bedb9
Dev Kordes <devkordes@gmail.com>
2019-11-12 21:07:00 -0500

    Implemented pages

--- resources/js/components/Category.js
+++ resources/js/components/Category.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {getCategory} from '../api/categories';
 
 const Category = () => {
 	const {slug} = useParams();
+	const [category, setCategory] = useState(null);
 
-    return (
-        <>
-            Category slug: {slug}
-        </>
-    );
+	useEffect(() => {
+		getCategory(slug)
+			.then(({data}) => {
+				setCategory(data.data);
+			})
+			.catch(console.log);
+	});
+
+    return category === null 
+    	? <>Loading...</>
+    	: <>
+    		<h1>{category.name}</h1>
+            {category.posts.map((post) => (
+            	<div key={post.slug}>
+            		<h2>{post.title}</h2>
+            		<p className="summary">{post.content.substring(0, 200)} ...</p>
+            		<em>{category.name}</em>
+            		<Link to={`/posts/${post.slug}`} className="read-more">Read</Link>
+            	</div>
+        	))}
+    	</>;
 }
 
 export default Category;
--- resources/js/components/Post.js
+++ resources/js/components/Post.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
+import {getPost} from '../api/posts';
 
 const Post = () => {
 	const {slug} = useParams();
+	const [post, setPost] = useState(null);
 
-    return (
-        <>
-            Post slug: {slug}
-        </>
-    );
+	useEffect(() => {
+		getPost(slug)
+			.then(({data}) => {
+				setPost(data.data);
+			})
+			.catch(console.log);
+	}, [slug]);
+
+    return post === null 
+    	? <>Loading...</>
+    	: <>
+	        	<h1>{post.title}</h1>
+	        	<p>{post.content}</p>
+	    </>;
 }
 
 export default Post;
